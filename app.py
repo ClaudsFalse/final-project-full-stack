b4ded79b7b3db96d86460247163c2fc367935bbe
Claudia <falsetticlaudia@gmail.com>
2023-04-25 21:52:08 +0100

    -

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import (
     url_for)
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import setup_db
+from backend.models import setup_db
 
 def create_app(test_config=None):
   # create and configure the app
