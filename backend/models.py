cfbe93fe3ce185688465acc814ea87de12924037
Claudia <falsetticlaudia@gmail.com>
2023-04-25 22:48:43 +0100

    -

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 
 import os
-from sqlalchemy import Column, String, create_engine, relationship
+from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
@@ -37,7 +37,7 @@ class Venue(db.Model):
     # facebook_link = db.Column(db.String(120))
     # seeking_talent = db.Column(db.Boolean)
     # seeking_description = db.Column(db.String(500))
-    productions = relationship('Production', backref='venue', lazy='joined', cascade="all, delete")
+    productions = db.relationship('Production', backref='venue', lazy='joined', cascade="all, delete")
 
 class Artist(db.Model):
     __tablename__ = 'artists'
