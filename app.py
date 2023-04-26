7dba24e1c5c99a18765df8a65d70bf4a3da72a71
Claudia <falsetticlaudia@gmail.com>
2023-04-26 16:10:59 +0100

    -

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from backend.models import setup_db
 from authlib.integrations.flask_client import OAuth
 from backend.auth.auth import AuthError, requires_auth
 from backend.utils import *
+from urllib.parse import quote_plus, urlencode
 
 def create_app(test_config=None):
   # create and configure the app
@@ -20,13 +21,42 @@ def create_app(test_config=None):
   setup_db(app)
   CORS(app, resources={r"/*": {"origins": "*"}})
 
-  # oauth = OAuth(app)
+  oauth = OAuth(app)
   
 
   @app.route('/')
   def get_main():
     LOGIN_URL = create_login_url()
     return render_template('index.html', session=session.get("user"), LOGIN_URL = LOGIN_URL)
+  
+  @app.route("/callback", methods=["GET", "POST"])
+  def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+  
+  @app.route("/login")
+  def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+  
+  @app.route("/logout")
+  def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
   
   # @app.route('/drinks-detail', methods=['GET'])
   # @requires_auth('get:drinks-detail')
@@ -46,6 +76,22 @@ def create_app(test_config=None):
   def login():
     return oauth.auth0.authorize_redirect(
         redirect_uri=url_for("callback", _external=True)
+    )
+  
+  @app.route("/logout")
+  def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
     )
 
   return app
