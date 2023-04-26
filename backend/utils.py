ab41b9b219ff7b547b5ee4d94c309fd49edbe557
Claudia <falsetticlaudia@gmail.com>
2023-04-26 11:16:07 +0100

    added create_url

--- backend/utils.py
+++ backend/utils.py
@@ -3,6 +3,7 @@ import os
 def create_login_url():
     url = 'https://'
     url = url + os.environ['AUTH0_DOMAIN'] + '/authorize?audience='+ os.environ['API_AUDIENCE']+'&response_type=token&client_id='+ os.environ['CLIENT_ID'] + '&redirect_uri='+'https://final-project-qnms.onrender.com'
+    return url
 
 
 
