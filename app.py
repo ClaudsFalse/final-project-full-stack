17544c51a46503cb9b5986e9c93807fc853f14d4
Claudia <falsetticlaudia@gmail.com>
2023-04-26 19:53:45 +0100

    added artists and venues view

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import os
+import json
 from flask import (
     Flask, 
+    abort,
     render_template, 
-    request, 
     session,
     flash, 
     redirect, 
     url_for)
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from backend.models import setup_db
+from flask_migrate import Migrate
+from backend.models import setup_db, Artist, Venue, db, db_drop_and_create_all
 from authlib.integrations.flask_client import OAuth
 from backend.auth.auth import AuthError, requires_auth
 from backend.utils import *
@@ -19,21 +21,36 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
+  migrate = Migrate(app, db)
+  db_drop_and_create_all()
+
+  # ROUTES
+  app.app_context().push()
   CORS(app, resources={r"/*": {"origins": "*"}})
+  app.secret_key = env.get("APP_SECRET_KEY")
 
   oauth = OAuth(app)
-  
 
-  @app.route('/')
-  def get_main():
-    LOGIN_URL = create_login_url()
-    return render_template('index.html', session=session.get("user"), LOGIN_URL = LOGIN_URL)
-  
-  @app.route("/callback", methods=["GET", "POST"])
-  def callback():
-    token = oauth.auth0.authorize_access_token()
-    session["user"] = token
-    return redirect("/")
+  oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+    )
+
+
+  @app.route("/")
+  def home():
+    data=session.get('user')
+
+    return render_template("index.html", 
+                           session=data,
+                           pretty=json.dumps(session.get('user'),
+                                             indent=4))
+
   
   @app.route("/login")
   def login():
@@ -41,12 +58,17 @@ def create_app(test_config=None):
         redirect_uri=url_for("callback", _external=True)
     )
   
+  @app.route("/callback", methods=["GET", "POST"])
+  def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+  
   @app.route("/logout")
   def logout():
     session.clear()
     return redirect(
-        "https://"
-        + env.get("AUTH0_DOMAIN")
+        "https://" + env.get("AUTH0_DOMAIN")
         + "/v2/logout?"
         + urlencode(
             {
@@ -56,21 +78,40 @@ def create_app(test_config=None):
             quote_via=quote_plus,
         )
     )
-
   
-  # @app.route('/drinks-detail', methods=['GET'])
-  # @requires_auth('get:drinks-detail')
+  @app.route("/account")
+  def get_user_account():
+    data=session.get('user')
+    if session:
+      email = data['userinfo']['email']
+      artist = Artist.query.filter(Artist.email == email).one_or_none()
+      if artist:
+        return render_template('view_artist_account.html', name=artist.name)
+      else:
+        return render_template('create_account.html')
+   
+
   @app.route('/productions')
   def get_productions():
     return render_template('productions.html')
   
   @app.route('/venues')
   def get_venues():
-    return render_template('venues.html')
+    try:
+      venue_query = Venue.query.all()
+      venues = [venue for venue in venue_query]
+    except:
+      abort(404)
+    return render_template('venues.html', venues=venues)
   
   @app.route('/artists')
   def get_artists():
-    return render_template('artists.html')
+    try:
+      artist_query = Artist.query.all()
+      artists = [artist for artist in artist_query]
+    except:
+      abort(404)
+    return render_template('artists.html', artists=artists)
   
   return app
 
--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,13 @@
-
-import os
+from os import environ as env
+from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
-import json
 
-database_path = os.environ['DATABASE_URL']
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+database_path = env['DATABASE_URL']
 if database_path.startswith("postgres://"):
   database_path = database_path.replace("postgres://", "postgresql://", 1)
 
@@ -17,6 +20,28 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+    # add one demo row which is helping in POSTMAN test
+    newArtist = Artist(name = 'Jordan',
+                   email = 'artist@groove.com',
+                   phone = '07563489658',
+                   genres = ['disco', 'house'])
+    
+    newVenue = Venue(name="Tabac", 
+                     genres=['disco', 'house'],
+                     address="Mitchell Lane",
+                     phone="01415721448",
+                     image_link="https://shorturl.at/vL789")
+    
+
+    items = [newArtist, newVenue]
+    for item in items:
+        db.session.add(item)
+        db.session.commit()
+
+
 
 '''
 Artist
@@ -28,34 +53,22 @@ class Venue(db.Model):
     id = Column(db.Integer, primary_key=True, autoincrement=True)
     name = Column(db.String)
     genres = Column(db.ARRAY(db.String), nullable=False)
-    # city = Column(db.String(120))
-    # state = Column(db.String(120))
     address = Column(db.String(120))
     phone = Column(db.String(120))
-    # website = Column(db.String(500))
     image_link = Column(db.String(500))
-    # facebook_link = db.Column(db.String(120))
-    # seeking_talent = db.Column(db.Boolean)
-    # seeking_description = db.Column(db.String(500))
-    productions = db.relationship('Production', backref='venue', lazy='joined', cascade="all, delete")
+    gigs = db.relationship('Gig', backref='venue', lazy='joined', cascade="all, delete")
 
 class Artist(db.Model):
     __tablename__ = 'artists'
 
     id = Column(db.Integer, primary_key=True)
     name = Column(db.String)
-    # city = Column(db.String(120))
-    # state = Column(db.String(120))
+    email = Column(db.String)
     phone = Column(db.String(120))
     genres = Column(db.ARRAY(db.String), nullable=False)
-    # image_link = db.Column(db.String(500))
-    # facebook_link = db.Column(db.String(120))
-    # seeking_venue= db.Column(db.Boolean)
-    # website = db.Column(db.String(500))
-    # seeking_description = db.Column(db.String(500))
-    productions = db.relationship('Production', backref='artist', lazy='joined', cascade="all, delete")
+    gigs = db.relationship('Gig', backref='artist', lazy='joined', cascade="all, delete")
 
-class Production(db.Model):
+class Gig(db.Model):
   __tablename__ = 'productions'
   id = Column(db.Integer, primary_key=True)
   # define foreign keys that map to the primary keys in the respective parent tables
