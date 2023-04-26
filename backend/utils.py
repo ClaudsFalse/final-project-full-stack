619f235cc05ededd0396642b2b3f24fac52866db
Claudia <falsetticlaudia@gmail.com>
2023-04-26 13:43:20 +0100

    -

--- backend/utils.py
+++ backend/utils.py
@@ -2,15 +2,7 @@ import os
 
 def create_login_url():
     url = 'https://'
-    url = url + os.environ['AUTH0_DOMAIN'] + '/authorize?audience='+ os.environ['API_AUDIENCE']+'&response_type=token&client_id='+ os.environ['CLIENT_ID'] + '&redirect_uri='+'https://final-project-qnms.onrender.com/'
+    url = url + os.environ['AUTH0_DOMAIN'] + '/authorize?audience='+ os.environ['API_AUDIENCE']+'&response_type=token&client_id='+ os.environ['CLIENT_ID'] + '&redirect_uri='+os.environ['AUTH0_URL']
     return url
 
 
-
-
-
-'''
-https://fullstackclaud.eu.auth0.com/authorize?audience=final-project&response_type=token&client_id=hPqZKjCikVhCWNUeUjYcHj5yn00txS4J&redirect_uri=https://final-project-qnms.onrender.com
-
-database_path = os.environ['DATABASE_URL']
-'''
