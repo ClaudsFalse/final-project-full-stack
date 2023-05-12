268355028ad9eb95d71bb797d813159b430d802a
Claudia <falsetticlaudia@gmail.com>
2023-05-13 00:00:34 +0100

    -

--- app.py
+++ app.py
@@ -223,7 +223,8 @@ def create_app(test_config=None):
         return render_template('new_gig.html')
   return app
 
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
+    # app = create_app()
     app.run(debug=True)
